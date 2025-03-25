acef341d140a90d0b52ea55a9bae9a98ac93294c
Stina Persson <stpe1901@student.miun.se>
2025-03-25 09:45:27 +0100

    Påbörjad lösning med nyhetsboxar. Ska läsa in innehåll dynamiskt. Inte helt löst

--- functions.php
+++ functions.php
@@ -1,1 +1,14 @@
 <?php
+
+function frost_child_register_block_patterns() {
+    register_block_pattern(
+        'frost-child/news-boxes-three',
+        array(
+            'title'       => __( 'Newsboxes with picture, heading, text, button', 'frost-child' ),
+            'description' => _x( 'A pattern with three news boxes, each containing a picture, heading, text, and a button.', 'Block pattern description', 'frost-child' ),
+            'content'     => file_get_contents( get_theme_file_path( '/patterns/news-boxes-three.php' ) ),
+            'categories'  => array( 'featured' ),
+        )
+    );
+}
+add_action( 'init', 'frost_child_register_block_patterns' );
